0400d82a1222809cea08e3ddaedd748149cb0de4
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-13 11:47:41 +0530

    Add files via upload

---
+++ listtoset.py
@@ -0,0 +1,8 @@
+#python progarm to convert a list into set
+
+#creating a list
+list1=["good","excellent","nice","amazing"]
+#converting a list to set
+listtoset=set(list1)
+print(listtoset)
+print(type(listtoset))
